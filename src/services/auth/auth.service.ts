20fe16d7a041234e272e116e410221dd665dc5b3
Wellyngton Teixeira dos Santos <wellyngton@uft.edu.br>
2019-04-29 16:48:42 -0300

    1.1.1 - Preparando filtragem por tipo de perfil

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,15 +3,23 @@ import {AngularFireAuth} from "@angular/fire/auth";
 import * as firebase from 'firebase/app';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/switchMap';
+import {User} from "../../models/user.model";
+import {AngularFireDatabase, AngularFireObject} from "@angular/fire/database";
 
 @Injectable()
 export class AuthService {
 
     user: firebase.User
+    usuario: User
 
-    constructor(public afAuth: AngularFireAuth) {
+    constructor(public afAuth: AngularFireAuth, private db: AngularFireDatabase) {
         afAuth.authState.subscribe(user => {
             this.user = user;
+            if(user){
+                this.db.object<User>("usuarios/"+user.uid).valueChanges().subscribe(
+                    sub=> this.usuario = sub
+                )
+            }
         });
     }
 
